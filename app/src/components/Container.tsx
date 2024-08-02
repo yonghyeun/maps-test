b5aa51d68351e312a9f8d0f9d140ec768fa3a262
yonghyeun <ttddcc119@naver.com>
2024-08-03 00:17:53 +0900

    feat : styled-component 테스트
    styled component가 SSR 로 잘 작동하여 넘어올 때 스타일이 적용된 채로
    넘어온다
    
    다만 스타일드 컴포넌트를 사용하는 모든 컴포넌트를 use client 로
    선언해줘야 하는 것일까? 현재로선 잘 모르겠는데 나중에 방법을 찾아보자
    
    feat [#1]

---
+++ app/src/components/Container.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Container = styled.section({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+});
+
+export default Container;
